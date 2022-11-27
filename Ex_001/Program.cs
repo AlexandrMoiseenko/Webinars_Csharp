11d20eae941ef8252a6cd9b161d76572c64d164a
AlexandrMoiseenko <moiseenko.112@mail.ru>
2022-11-27 15:27:40 +0300

    Программа по нахождению второй цифры из трехзначного числа

---
+++ Ex_001/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число и на выходе
+// показывет ВТОРУЮ цифру этого числа. Например:
+// 456 -> 5
+
+int EnterNumber(string message)      // Метод по вводу числа (из терминала)
+{
+    System.Console.Write(message + ": ");
+    int input = Convert.ToInt32(Console.ReadLine());
+    return input;
+}
+
+bool ValidateNumber (int number)        // Функция на провреку трехзначного числа
+{
+    if (number > 99 && number < 1000)
+    {
+        return true;
+    }
+
+    Console.WriteLine("Число не является трехзначным");
+    return false;
+}
+
+int number = EnterNumber("Введите трехзначное число ");     // Вводим число с помощью функции
+if (ValidateNumber(number))
+{
+    int PromResult = number / 10;
+    int result = PromResult % 10;
+    Console.WriteLine("Второй цифрой числа " + number + " является - " + result);
+}
